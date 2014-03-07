611060fca59e6d5e81fb371dcd366f4b7ae1c963
Young Ho Shin <yhshin11@gmail.com>
2014-03-07 16:22:37 -0500

    sub - Added angle1, angle2, Dy and Dx2 couts.

--- src/LYSimDetectorConstruction.cc
+++ src/LYSimDetectorConstruction.cc
@@ -221,6 +221,10 @@ G4VPhysicalVolume* LYSimDetectorConstruction::ConstructDetector()
  * 		G4cout << "Dx2 set to " << G4BestUnit(Dx2, "Length") << G4endl;
  * 	}
  */
+	G4cout << "angle1 set to " << angle1 << G4endl;
+	G4cout << "angle2 set to " << angle2 << G4endl;
+	G4cout << "Dy set to " << G4BestUnit(Dy, "Length") << G4endl;
+	G4cout << "Dx2 set to " << G4BestUnit(Dx2, "Length") << G4endl;
 	
 	G4VSolid* solidTile = 
 	ConstructTileSolid("TileTrap", 
